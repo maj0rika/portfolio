8e36368b8c7d7aacf3dd5bb97d13bfb6c11facf0
마조리카 <47102222+neu5563@users.noreply.github.com>
2023-06-12 01:58:40 +0900

    feat: 사이드 네비게이션 바 추가

---
+++ components/SideNav.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav>
+    <figure>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <!-- 앞쪽 이미지 -->
+            <img src="~/assets/images/logo.png" alt="logo" />
+          </div>
+          <div class="flip-card-back">
+            <!-- 뒤쪽 이미지 -->
+            <img src="~/assets/images/me.jpeg" alt="logo" />
+          </div>
+        </div>
+      </div>
+    </figure>
+
+    <ul>
+      <li><p to="/">HOME</p></li>
+      <li><p to="/about">ABOUT</p></li>
+      <li><p to="/tech-skills">TECH SKILLS</p></li>
+      <li><p to="/projects">PROJECTS</p></li>
+      <li><p to="/experience">EXPERIENCE</p></li>
+      <li><p to="/career">CAREER</p></li>
+      <li><p to="/education">EDUCATION</p></li>
+      <li><p to="/blog">BLOG</p></li>
+      <li><p to="/contact">CONTACT</p></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+  nav {
+    width: 100%;
+    height: 100%;
+    padding: 3rem 0;
+
+    figure {
+      display: flex;
+      height: 22rem;
+
+      img {
+        width: 100%;
+        height: auto;
+      }
+      .flip-card {
+        display: flex;
+        width: 100%;
+        perspective: 1000px;
+        transition: transform 0.6s;
+
+        .flip-card-inner {
+          position: relative;
+          width: 100%;
+          height: 100%;
+          transform-style: preserve-3d;
+          transition: transform 0.6s;
+          &:hover {
+            transform: rotateY(180deg);
+          }
+        }
+      }
+
+      .flip-card-front,
+      .flip-card-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+      }
+
+      .flip-card-front {
+        z-index: 1;
+        transform: rotateY(0deg);
+      }
+
+      .flip-card-back {
+        transform: rotateY(180deg);
+      }
+    }
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100%;
+      padding: 0;
+      font-size: 1.2rem;
+      list-style-type: none;
+
+      gap: 2rem;
+
+      li {
+        display: flex;
+        //li 밑줄 제거
+
+        p {
+          cursor: pointer;
+          position: relative;
+          display: flex;
+          color: #000000b3;
+          text-decoration: none;
+
+          &::after {
+            content: '';
+            position: absolute;
+            bottom: 0.3rem;
+            left: 50%;
+            width: 0;
+            height: 0.1rem;
+            background-color: #862ddff2; /* 언더라인 색상을 원하는 색으로 변경하세요 */
+            transform: translateX(-50%);
+            transition: all 0.1s ease-in-out;
+          }
+          &:hover {
+            color: black;
+            font-weight: 500;
+          }
+          &:hover::after {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+</style>
