9e8c5b5ceacfdb5b37841c8d553f98f7f46105ed
마조리카 <47102222+neu5563@users.noreply.github.com>
2023-06-12 01:58:23 +0900

    feat: 기본 레이아웃 추가

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout">
+    <SideNav />
+    <main><slot /></main>
+    <Footer />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .layout {
+    display: grid;
+    grid-template-columns: 30rem 1fr;
+    grid-template-rows: 1fr auto;
+    grid-template-areas:
+      'side main'
+      'side footer';
+    height: 100vh;
+  }
+</style>
