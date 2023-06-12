f0628dc535b7541d31dc705e1d02b1b11d71594b
마조리카 <neu5563@naver.com>
2023-06-12 18:05:02 +0900

    feat: wave text 컴포넌트 추가
    props로 텍스트, 반복횟수

---
+++ components/Input/WaveText.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wave-text">
+    <span
+      v-for="(char, index) in text"
+      :key="index"
+      :style="{
+        animationDelay: `${index * 0.3}s`,
+        animationIterationCount: repeat,
+      }"
+      class="wave-animation"
+    >
+      {{ char }}
+      <template v-if="char == ' '">&nbsp;</template>
+    </span>
+  </div>
+</template>
+
+<script setup>
+  // const text = ref('Hello, World!')
+
+  const props = defineProps({
+    text: {
+      type: String,
+      default: 'Hello, World!',
+    },
+    repeat: {
+      type: Number,
+      default: 3,
+    },
+  })
+</script>
+
+<style scoped lang="scss">
+  @keyframes wave {
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-50%);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+
+  .wave-text {
+    display: flex;
+    width: 100%;
+    height: fit-content;
+  }
+
+  .wave-animation {
+    display: flex;
+    flex-flow: row;
+    animation: wave 2s;
+  }
+</style>
