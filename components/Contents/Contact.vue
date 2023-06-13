9438c055485096cde5772f34fa3ae05a2e615b04
마조리카 <neu5563@naver.com>
2023-06-13 18:23:26 +0900

    feat: contact에 아이콘 추가

--- components/Contents/Contact.vue
+++ components/Contents/Contact.vue
@@ -5,8 +5,64 @@
     <template v-slot:title>Contact</template>
     <template v-slot:subTitle></template>
 
-    <template v-slot:body></template>
+    <template v-slot:body>
+      <section>
+        <article>
+          <div class="icon-box">
+            <Icon class="icon" name="heroicons:at-symbol" />
+          </div>
+          <p>ddd</p>
+        </article>
+        <article>
+          <div class="icon-box">
+            <Icon class="icon" name="heroicons:map-pin" />
+          </div>
+        </article>
+
+        <article>
+          <div class="icon-box">
+            <Icon class="icon" name="heroicons:phone-arrow-down-left" />
+          </div>
+        </article>
+        <!-- <article>
+          <div class="icon-box">
+            <Icon class="icon" name="heroicons:at-symbol" />
+          </div>
+        </article> -->
+      </section>
+    </template>
   </ContentsContainer>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  section {
+    display: flex;
+    flex-flow: column;
+    width: 100%;
+
+    gap: 2rem;
+
+    article {
+      display: flex;
+      align-items: center;
+      gap: 4rem;
+
+      p {
+        font-size: 2rem;
+      }
+    }
+  }
+
+  .icon-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 12rem;
+    height: 12rem;
+    background-color: #862ddfb8;
+  }
+  .icon {
+    width: 10rem;
+    height: 10rem;
+  }
+</style>
