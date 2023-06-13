a27204b8e2c118af9bef08eefd173aba03be215c
마조리카 <neu5563@naver.com>
2023-06-13 15:49:56 +0900

    feat: 컨텐츠들을 감싸는 컨테이너 컴포넌트 생성
    같은 디자인 공유를 위한 컴포넌트
    solt을 통해 바인딩

---
+++ components/Contents/Container/Index.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="container">
+    <header class="header">
+      <div class="title">
+        <slot name="title" />
+      </div>
+      <div class="subTitle">
+        <slot name="subTitle" />
+      </div>
+    </header>
+    <article class="body">
+      <slot name="body" />
+    </article>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  .container {
+    //TODO: 테스트 지우기
+    min-height: 100vh; //test
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    gap: 5rem;
+  }
+
+  .header {
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    align-items: flex-start;
+    gap: 2rem;
+
+    .title {
+      color: #888888;
+      display: flex;
+      font-size: 1.5rem;
+      font-weight: 700;
+    }
+
+    .subTitle {
+      display: flex;
+      font-size: 2.5rem;
+      font-weight: 700;
+      color: #862ddfb8;
+    }
+  }
+
+  .body {
+    display: flex;
+    flex-flow: column;
+    gap: 2rem;
+
+    font-size: 2rem;
+  }
+</style>
