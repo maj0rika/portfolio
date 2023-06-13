f7f39e454ec1475756ba16963c61dca08c7c3324
마조리카 <neu5563@naver.com>
2023-06-13 16:09:40 +0900

    fix: 롤백

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,46 @@
 <script setup lang="ts">
   const route = useRoute()
   const toggleSidebar = ref(false)
+  const toggleProfileImage = ref(false)
 </script>
 
 <template>
-  <div class="layout">
-    <Icon
-      name="heroicons:bars-3"
-      size="36"
-      color="#252530"
-      class="mobile"
-      @click="toggleSidebar = !toggleSidebar"
-    />
-    <SideNav class="side" v-model:isShow="toggleSidebar" />
+  <div class="container" ref="el" :style="{}">
+    <div class="layout">
+      <Icon
+        name="heroicons:bars-3"
+        size="36"
+        color="#252530"
+        class="mobile"
+        @click="toggleSidebar = !toggleSidebar"
+      />
+      <SideNav class="side" v-model:isShow="toggleSidebar" />
 
-    <main class="main">
-      <NuxtPage :page-key="route.fullPath" />
-    </main>
-    <Footer />
+      <main class="main">
+        <NuxtPage :page-key="route.fullPath" />
+      </main>
+      <Footer />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
   @import 'assets/styles/index.scss';
-
-  .layout {
-    padding: 3rem;
-    background-color: #f5f5f5;
-    width: calc(100% - 6rem);
-    height: calc(100% - 6rem); // sticky 적용하려면 height 필요
-    min-height: 100vh;
+  .container {
     display: flex;
     flex-flow: column;
     justify-content: center;
     align-items: center;
+    width: calc(100% - 6rem);
+    height: calc(100% - 6rem); // sticky 적용하려면 height 필요
+    min-height: 100vh;
+    padding: 3rem;
+    background-color: #f5f5f5;
+
+    touch-action: auto;
+  }
+  .layout {
     @include mobile-over {
-      position: relative;
       display: grid;
       width: 100%;
       max-width: 102.4rem;
@@ -49,11 +54,6 @@
     }
     @include mobile {
     }
-  }
-
-  .main {
-    display: flex;
-    width: 100%;
   }
 
   .side {
