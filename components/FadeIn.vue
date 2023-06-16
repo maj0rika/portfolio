8b4629b8833f41a05144ca9cf0afe2b25927c5d4
마조리카 <neu5563@naver.com>
2023-06-16 18:01:55 +0900

    feat: 애니메이션용 컴포넌트 추가

--- components/FadeIn.vue
+++ components/FadeIn.vue
@@ -1,32 +1,68 @@
-<script setup>
+<script setup lang="ts">
   import { useIntersectionObserver } from '@vueuse/core'
+  import { TFadeIn } from '~/types'
 
-  const fadeElement = ref(null)
+  // const value = inject<TFadeIn['mode']>('fadeMode', 'left')
+
+  // const props = defineProps<{
+  //   mode: TFadeIn['mode']
+  // }>()
+
+  const props = withDefaults(
+    defineProps<{
+      mode: TFadeIn['mode']
+    }>(),
+    {
+      mode: 'left',
+    },
+  )
+
+  const toggleFade = ref(false)
+  const fadeElement = ref<HTMLElement | null>(null)
+
   useIntersectionObserver(fadeElement, ([{ isIntersecting }]) => {
-    console.log(isIntersecting)
-    if (isIntersecting) {
-      fadeElement.value.classList.add('show')
-    } else {
-      fadeElement.value.classList.remove('show')
-    }
+    if (!fadeElement.value) return
+
+    toggleFade.value = isIntersecting
+  })
+
+  const fadeDirections: Record<TFadeIn['mode'], string> = {
+    left: 'translate3d(-50vw, 0, 0)',
+    right: 'translate3d(50vw, 0, 0)',
+    up: 'translate3d(0, 50%, 0)',
+    down: 'translate3d(0, -50%, 0)',
+  }
+
+  const fadeMode = computed(() => {
+    // return fadeDirections[value] || fadeDirections['left']
+    return fadeDirections[props.mode] || fadeDirections['left']
   })
 </script>
 
 <template>
-  <div>
-    <div ref="fadeElement" class="fade-in">
-      <slot />
-    </div>
+  <div
+    ref="fadeElement"
+    :style="{
+      transition: 'all 1s ease-in-out',
+      transform: fadeMode,
+    }"
+    :class="{
+      'fade-in': toggleFade,
+      'fade-out': !toggleFade,
+    }"
+  >
+    <slot />
   </div>
 </template>
 
 <style scoped>
   .fade-in {
-    opacity: 0;
-    transition: opacity 0.5s ease-in;
+    opacity: 1;
+    transform: translate3d(0, 0, 0) !important;
   }
 
-  .fade-in.show {
-    opacity: 1;
+  .fade-out {
+    opacity: 0;
+    /* transform: translate3d(50vw, 0, 0); */
   }
 </style>
