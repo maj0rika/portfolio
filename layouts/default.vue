dfc013349096a76c2e82c74d77b5b065c0793149
마조리카 <neu5563@naver.com>
2023-06-19 16:39:40 +0900

    fix: layout 구성 변경

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,19 @@
 
 <template>
   <div class="container">
-    <div class="layout">
+    <div
+      :style="{
+        display: 'flex',
+
+        alignItems: 'center',
+        justifyContent: 'center',
+        // width: '100%',
+        height: '100%',
+        margin: '0',
+        padding: '3rem',
+        maxWidth: '102.4rem',
+      }"
+    >
       <div class="icon-box">
         <Icon
           name="heroicons:bars-3"
@@ -21,76 +33,51 @@
       </div>
       <SideNav class="side" v-model:isShow="toggleSidebar" />
 
-      <main class="main">
-        <slot />
-      </main>
-      <Footer />
+      <div class="layout">
+        <slot :key="route.fullPath" />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
   @import 'assets/styles/index.scss';
+
   .container {
+    // overflow-y: hidden;
     display: flex;
     flex-flow: column;
-    justify-content: center;
     align-items: center;
-    // overflow: hidden;
-    width: calc(100vw - 6rem);
-    height: calc(100% - 6rem); // sticky 적용하려면 height 필요
-    min-height: 100vh;
-    padding: 3rem;
-    background-color: #f5f5f5;
-
-    touch-action: auto;
+    width: 100%;
+    height: 100%;
+    margin: 0;
   }
+
   .layout {
-    @include mobile-over {
-      display: grid;
-      width: 100%;
-      max-width: 102.4rem;
-      height: 100%;
-      grid-gap: 1rem;
-      grid-template-areas:
-        'side main'
-        'side footer';
-      grid-template-columns: 30rem 1fr;
-      grid-template-rows: 1fr auto;
-    }
-
     @include mobile {
-      display: flex;
-      flex-flow: column;
-      width: 100%;
-      height: 100%;
+      flex-direction: column;
     }
-  }
-
-  .side {
     @include mobile-over {
-      position: sticky;
-      top: 0;
-      align-self: start;
+      flex-direction: row;
     }
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    // overflow: hidden;
+
+    gap: 2rem;
   }
 
   .icon-box {
-    @include mobile-over {
-      display: none;
+    display: none;
+
+    @include mobile {
+      z-index: 101;
+      display: flex;
+      position: fixed;
+      top: 1rem;
+      left: 1rem;
     }
-    position: fixed;
-    top: 1rem;
-    left: 1rem;
-    z-index: 999;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: fit-content;
-    height: fit-content;
-    padding: 0.5rem;
-    background-color: #f5f5f5;
-    border: 0.1rem solid #252530;
-    border-radius: 20%;
   }
 </style>
