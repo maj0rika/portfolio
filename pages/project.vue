97e3d522c9d22ef69bd2334dc96759c500efabe6
마조리카 <neu5563@naver.com>
2023-06-21 18:32:25 +0900

    creat: page/project 추가

---
+++ pages/project.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <article>
+    <NuxtPage :page-key="$route.fullPath" />
+  </article>
+</template>
+
+<style scoped lang="scss">
+  article {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    // align-items: center;
+    min-height: 100vh;
+  }
+</style>
