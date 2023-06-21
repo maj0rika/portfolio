1c93851ac537bafb5ca19afae187956602dd8d68
마조리카 <neu5563@naver.com>
2023-06-21 18:31:48 +0900

    feat: 로지파스타 상세페이지 추가

---
+++ pages/project/quick.vue
@@ -0,0 +1,144 @@
+<script setup lang="ts">
+  const modal = useModal()
+</script>
+
+<template>
+  <ProjectContainer>
+    <template v-slot:name>로지파스타 22.02. - 22.10.</template>
+    <template v-slot:introduction>물류,퀵 접수 온라인 전환 솔류션</template>
+    <template v-slot:features>
+      vue2, vue3, nuxt3, veutify, tailwindCss, pinia, scss
+    </template>
+    <template v-slot:contents>
+      <div class="flex-box" style="gap: 0px">
+        <p>- 툴팁 및 card 형태 재사용 가능한 컴포넌트 설계 및 기능 추가</p>
+        <p>- 커스텀 스킨 제작 및 기존 서비스와 연동</p>
+        <p>
+          - ADMIN 페이지 커스텀 스킨 설정 UIUX 개편으로 스킨 설정 관련 고객 문의
+          50% 감소
+        </p>
+      </div>
+      <div class="flex-box center">
+        <ProjectCard :flow="'row'">
+          <template v-slot:img>
+            <img
+              @click="
+                modal.open('COMMENT', {
+                  content: '/_nuxt/assets/images/logipasta/미리보기.gif',
+                })
+              "
+              src="~/assets/images/logipasta/미리보기.png"
+              alt="image"
+            />
+          </template>
+          <template v-slot:content>
+            <div class="flex-box">
+              <h2>스킨</h2>
+              <p>스킨 변경사항을 미리보기 할수있는 편의성 제공</p>
+              <p>클릭으로 해당 부분 이미지 변경이 바로 가능하도록 적용</p>
+              <p>
+                변경사항이 실시간으로 반영되어 고객이 변경된 부분을 바로
+                인식할수 있도록 제공
+              </p>
+              <p>기존 대비 고객문의 50%이상 감소</p>
+            </div>
+          </template>
+        </ProjectCard>
+        <ProjectCard :flow="'row'">
+          <template v-slot:img>
+            <img
+              @click="
+                modal.open('COMMENT', {
+                  content: '/_nuxt/assets/images/logipasta/팝업.gif',
+                })
+              "
+              src="~/assets/images/logipasta/팝업.png"
+              alt="image"
+            />
+          </template>
+          <template v-slot:content>
+            <div class="flex-box">
+              <h2>커스텀 팝업기능</h2>
+              <p>관리자가 직접 공지사항 팝업을 작성 할수있는 기능</p>
+              <p>색상, 이미지, 링크, 여백등 다양한 커스텀 기능 제공</p>
+              <p>미리보기를 제공하여 공지사항 작성에 용이하게 제작</p>
+              <p>api와 toastUi editor를 활용하여 서비스페이지에 전달</p>
+            </div>
+          </template>
+        </ProjectCard>
+        <ProjectCard :flow="'row'">
+          <template v-slot:img>
+            <img
+              @click="
+                modal.open('COMMENT', {
+                  content: '/_nuxt/assets/images/logipasta/주문UI.gif',
+                })
+              "
+              src="~/assets/images/logipasta/주문UI.png"
+              alt="image"
+            />
+          </template>
+          <template v-slot:content>
+            <div class="flex-box">
+              <h2>주문내역 UI 리뉴얼</h2>
+              <p>기존에 없던 자세히 기능 추가</p>
+              <p>토글을 통해 자세히 보기와 간단히 보기 변경</p>
+              <p>주문내역에서 거래처 등록 가능하게 기능 추가</p>
+            </div>
+          </template>
+        </ProjectCard>
+        <ProjectCard :flow="'row'">
+          <template v-slot:img>
+            <img
+              @click="
+                modal.open('COMMENT', {
+                  content: '/_nuxt/assets/images/logipasta/요금계산.gif',
+                })
+              "
+              src="~/assets/images/logipasta/요금계산.png"
+              alt="image"
+            />
+          </template>
+          <template v-slot:content>
+            <div class="flex-box">
+              <div class="flex-box">
+                <h2>요금 테스트 기능 추가</h2>
+                <p>
+                  관리자가 설정한 요금체계로 동작하는지 확인할 수 있는 테스트
+                  페이지
+                </p>
+                <p>다양한 설정들이 설정되었는지 궁금한 니즈에서 시작</p>
+                <!-- <p>주문내역에서 거래처 등록 가능하게 기능 추가</p> -->
+              </div>
+            </div>
+          </template>
+        </ProjectCard>
+        <div class="flex-box"><p style="font-size: 3.2rem">배운점</p></div>
+      </div>
+    </template>
+  </ProjectContainer>
+</template>
+
+<style scoped lang="scss">
+  @import 'assets/styles/index.scss';
+
+  .flex-box {
+    display: flex;
+    flex-flow: column;
+    width: 100%;
+    height: 100%;
+    gap: 3rem;
+
+    &.center {
+      @include mobile-over {
+        align-items: center;
+      }
+    }
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+</style>
