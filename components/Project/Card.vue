06f4069ffa87e3805695e2b594763eec66b420a8
마조리카 <neu5563@naver.com>
2023-06-21 18:31:28 +0900

    feat: 프로젝트 상세 페이지, 재사용 컴포넌트 추가

---
+++ components/Project/Card.vue
@@ -0,0 +1,106 @@
+import { Base } from '../../.nuxt/components';
+<script setup lang="ts">
+  const props = withDefaults(
+    defineProps<{
+      flow: 'row' | 'column'
+    }>(),
+    {
+      flow: 'row',
+    },
+  )
+</script>
+
+<template>
+  <div
+    :class="{
+      // 'wrapper-column': props.flow === 'column',
+      // 'wrapper-row': props.flow === 'row',
+      wrapper: true,
+    }"
+  >
+    <div class="img-wrapper">
+      <div class="card-image">
+        <slot name="img" />
+      </div>
+    </div>
+    <div class="content">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @import 'assets/styles/index.scss';
+  // .wrapper-column {
+  //   display: flex;
+  //   flex-flow: column;
+  //   align-items: center;
+  //   flex: 1 1;
+  //   width: 100%;
+  //   height: 100%;
+  // }
+
+  // .wrapper-row {
+  //   display: grid;
+  //   flex: 1;
+  //   width: 100%;
+  //   height: 100%;
+
+  //   gap: 2rem;
+  //   grid-template-columns: 1fr 2fr;
+  // }
+
+  .wrapper {
+    @include mobile {
+      display: flex;
+      flex-flow: column;
+      align-items: center;
+      flex: 1 1;
+      width: 100%;
+      height: 100%;
+    }
+    @include mobile-over {
+      display: grid;
+      flex: 1;
+      width: 100%;
+      height: 100%;
+
+      grid-template-columns: 1fr 2fr;
+    }
+
+    gap: 0.5rem;
+  }
+  .img-wrapper {
+    display: flex;
+    justify-content: center;
+    // align-items: center;
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    border: 0.1rem solid transparent;
+  }
+  .card-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    overflow: hidden;
+    width: 100%;
+    height: fit-content;
+    border: 0.1rem solid transparent;
+    border-radius: 1rem;
+    cursor: pointer;
+
+    object-fit: cover;
+  }
+
+  .content {
+    display: flex;
+    flex: 1;
+    width: calc(100% - 3rem);
+    padding: 1rem;
+    word-break: break-all;
+    border: 0.1rem solid #adc344;
+    border-radius: 1rem;
+  }
+</style>
---
+++ components/Project/Container.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="wrapper">
+    <h1 class="name">
+      <slot name="name" />
+    </h1>
+    <h3 class="introduction"><slot name="introduction" /></h3>
+    <h4 class="features">
+      <slot name="features" />
+    </h4>
+    <div class="contemnt-wrapper">
+      <slot name="contents" />
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  .wrapper {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    gap: 1rem;
+
+    // .name {
+    //   font-size: 3rem;
+    //   font-weight: 700;
+    // }
+
+    // .introduction {
+    //   font-size: 2rem;
+    //   font-weight: 700;
+    // }
+
+    // .features {
+    //   font-size: 2rem;
+    //   font-weight: 700;
+    // }
+  }
+
+  .contemnt-wrapper {
+    display: flex;
+    flex-flow: column;
+    // justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+
+    gap: 2rem;
+  }
+
+  // h1 {
+  //   color: #888;
+  //   font-size: 3rem;
+  //   font-weight: 700;
+  //   // margin: 0px !important;
+  // }
+  // h2 {
+  //   color: #888;
+  //   font-size: 2rem;
+  //   font-weight: 700;
+  //   // margin: 0px !important;
+  // }
+</style>
