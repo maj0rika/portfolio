f5b5f623d4a01d2cdc113261c75fe0c61d64aea1
마조리카 <neu5563@naver.com>
2023-06-27 01:12:12 +0900

    build: SEO 적용

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const route = useRoute()
+const head = ref<any>()
 
 watch(
   () => route.fullPath,
@@ -10,16 +11,120 @@ watch(
   },
   { deep: true },
 )
+
+const structuredData = {
+  '@context': 'http://schema.org',
+  '@type': 'WebPage',
+  'name': 'maj0rika',
+  'url': 'https://maj0rika.com',
+  'description': 'maj0rika portfolio'
+}
+
+head.value = {
+  title: 'maj0rika',
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://maj0rika.com',
+    },
+    {
+      rel: 'icon',
+      href: '/favicon.png',
+    },
+  ],
+  meta: [
+    {
+      name: 'title',
+      content: 'maj0rika',
+    },
+    {
+      name: 'author',
+      content: 'maj0rika',
+    },
+    {
+      name: 'description',
+      content: 'maj0rika portfolio',
+    },
+    {
+      name: 'keywords',
+      content:
+        '프론트엔드, frontend, front-end, 프론트엔드 포트폴리오, 포트폴리오, 개발자, 개발자 포폴, 프론트엔드 개발자, 프론트엔드 개발자 포트폴리오',
+    },
+    {
+      property: 'og:site_name',
+      content: 'maj0rika',
+    },
+    {
+      property: 'og:title',
+      content: 'maj0rika',
+    },
+    {
+      property: 'og:description',
+      content: 'maj0rika portfolio',
+    },
+    {
+      property: 'og:url',
+      content: 'https://maj0rika.com',
+    },
+    {
+      property: 'og:image',
+      content: '/favicon.png',
+    },
+    {
+      property: 'og:image:width',
+      content: '500',
+    },
+    {
+      property: 'og:image:height',
+      content: '500',
+    },
+  ],
+
+  __dangerouslyDisableSanitizersByTagID: {
+    structuredData: ['innerHTML']
+  }
+}
+
+const addStructuredData = () => {
+  head.value.script = [
+    {
+      innerHTML: JSON.stringify(structuredData),
+      type: 'application/ld+json',
+      id: 'structuredData'
+    }
+  ]
+}
+
+onMounted(addStructuredData)
 </script>
 
 <template>
-  <NuxtLayout>
-    <Loading />
+  <Html :lang="'ko-KR'" dir="ltr">
 
-    <NuxtPage :page-key="route.fullPath" />
-  </NuxtLayout>
+  <Head>
+    <Title>{{ head.title }}</Title>
+    <template v-for="link in head.link" :key="link.rel">
+      <Link :rel="link.rel" :href="link.href" />
+    </template>
+    <Meta charset="utf-8" />
+    <Meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <template v-for="meta in head.meta" :key="meta.name || meta.property">
+      <Meta v-if="meta.name" :name="meta.name" :content="meta.content" />
+      <Meta v-if="meta.property" :property="meta.property" :content="meta.content" />
+    </template>
+  </Head>
+
+  <Body>
+    <NuxtLayout>
+      <NuxtPage :page-key="route.fullPath" />
+    </NuxtLayout>
+  </Body>
   <Modal />
+  <MouseEffect />
+
+  </Html>
 </template>
+
 
 <style lang="scss">
 @import 'assets/styles/index.scss';
@@ -32,6 +137,31 @@ watch(
   @include mobile-over {
     display: none !important;
   }
+}
+
+@font-face {
+  font-family: 'LotteMartDream';
+  font-style: normal;
+  font-weight: 400;
+  src: url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamMedium.woff2') format('woff2'), url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamMedium.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'LotteMartDream';
+  font-style: normal;
+  font-weight: 700;
+  src: url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamBold.woff2') format('woff2'), url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamBold.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'LotteMartDream';
+  font-style: normal;
+  font-weight: 300;
+  src: url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamLight.woff2') format('woff2'), url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartDream/LotteMartDreamLight.woff') format('woff');
+}
+
+.lottemartdream * {
+  font-family: 'LotteMartDream', sans-serif;
 }
 
 @font-face {
@@ -54,7 +184,7 @@ body {
   font-size: 62.5% !important;
   line-height: 1.5 !important;
   vertical-align: middle !important;
-  font-family: 'Quicksand', Arial, sans-serif !important;
+  font-family: 'LotteMartDream', 'Quicksand', Arial, sans-serif !important;
 
   .flex-box {
     display: flex;
