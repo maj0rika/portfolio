4796d0278dd32e8406b98e95b406da683deaec3b
마조리카 <neu5563@naver.com>
2023-06-27 01:17:36 +0900

    feat: 마우스 애니메이션 추가

---
+++ components/MouseEffect.client.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+
+const mouseX = ref(0)
+const mouseY = ref(0)
+const bigX = ref(0)
+const bigY = ref(0)
+const smallX = ref(0)
+const smallY = ref(0)
+
+const updateMousePosition = (e: MouseEvent) => {
+  mouseX.value = e.clientX
+  mouseY.value = e.clientY
+}
+
+const animate = () => {
+  bigX.value += (mouseX.value - bigX.value) * 0.1
+  bigY.value += (mouseY.value - bigY.value) * 0.1
+
+  smallX.value += (mouseX.value - smallX.value) * 0.05
+  smallY.value += (mouseY.value - smallY.value) * 0.05
+
+  requestAnimationFrame(animate)
+}
+
+onMounted(() => {
+  window.addEventListener('mousemove', updateMousePosition)
+  animate()
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('mousemove', updateMousePosition)
+})
+
+</script>
+
+<template>
+  <img class="circle big" src="~/assets/images/logo.png" alt="logo" :style="{ top: `${bigY}px`, left: `${bigX}px` }" />
+  <img class="circle small" src="~/assets/images/logo.png" alt="logo"
+    :style="{ top: `${smallY}px`, left: `${smallX}px` }" />
+</template>
+
+<style scoped>
+.circle {
+  position: fixed;
+  z-index: 100;
+  cursor: none;
+  pointer-events: none;
+}
+
+.big {
+  width: 3rem;
+  height: 3rem;
+}
+
+.small {
+  width: 2rem;
+  height: 2rem;
+}
+</style>
