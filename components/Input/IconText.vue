524f458310122fca7eb5703998c706f95bc5a234
마조리카 <neu5563@naver.com>
2023-06-29 15:59:28 +0900

    Create IconText.vue

---
+++ components/Input/IconText.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    icon: {
+      name: string
+      size?: string
+      color?: string
+    }
+  }>()
+</script>
+
+<template>
+  <div class="wrapper">
+    <Icon
+      :name="icon.name"
+      class="icon"
+      :size="icon.size"
+      :color="icon.color"
+    />
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .wrapper {
+    display: flex;
+    flex-wrap: nowrap;
+    gap: 0.5rem;
+  }
+  .icon {
+    pointer-events: none;
+    display: flex;
+    margin-top: 0.2rem;
+    margin-bottom: auto;
+    flex-shrink: 0;
+  }
+</style>
