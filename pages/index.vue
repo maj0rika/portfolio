5e91f003a51c1c9349bae01838197a4dd648368d
마조리카 <neu5563@naver.com>
2023-07-07 17:52:27 +0900

    delete: 잠시 개인정보 가리기

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-const scrollTo = storeToRefs(useScrollTo())
-const sections = [
-  'home',
-  'about',
-  'techSkills',
-  'career',
-  'blog',
-  'experience',
-  'education',
-  'projects',
-  'contact',
-]
-const refs = <any>{}
+  const scrollTo = storeToRefs(useScrollTo())
+  const sections = [
+    'home',
+    'about',
+    'techSkills',
+    'career',
+    'blog',
+    'experience',
+    'education',
+    'projects',
+    'contact',
+  ]
+  const refs = <any>{}
 
-sections.forEach(section => {
-  refs[section] = ref(null)
-})
-
-onMounted(() => {
   sections.forEach(section => {
-    refs[section].value = document.getElementById(section)
-    scrollTo.ref.value[section] = refs[section].value
+    refs[section] = ref(null)
   })
-})
+
+  onMounted(() => {
+    sections.forEach(section => {
+      refs[section].value = document.getElementById(section)
+      scrollTo.ref.value[section] = refs[section].value
+    })
+  })
 </script>
 
 <template>
@@ -35,18 +35,18 @@ onMounted(() => {
     <ContentsBlog id="blog" />
     <!-- <ContentsExperience id="experience" /> -->
     <!-- <ContentsEducation id="education" /> -->
-    <ContentsContact id="contact" />
+    <!-- <ContentsContact id="contact" /> -->
   </article>
 </template>
 
 <style scoped lang="scss">
-article {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-  gap: 5rem;
-  width: 100%;
-  height: 100%;
-}
+    gap: 5rem;
+    width: 100%;
+    height: 100%;
+  }
 </style>
