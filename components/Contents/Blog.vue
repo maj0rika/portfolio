7942d97df08bd39a2352e54fde57ee64b60a7126
마조리카 <neu5563@naver.com>
2023-07-09 19:25:02 +0900

    delete: fade 애니메이션 줄이기

--- components/Contents/Blog.vue
+++ components/Contents/Blog.vue
@@ -1,26 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <FadeIn :mode="'left'">
-    <ContentsContainer>
-      <template v-slot:title>BLOG</template>
-      <template v-slot:subTitle>내 블로그는 이렇게 있어요</template>
+  <ContentsContainer>
+    <template v-slot:title>BLOG</template>
+    <template v-slot:subTitle>내 블로그는 이렇게 있어요</template>
 
-      <template v-slot:body>
-        <FadeIn :mode="'right'">
-          <NuxtLink to="https://maj0rika.com/react">
-            REACT 블로그로 이동
-          </NuxtLink>
-        </FadeIn>
-        <FadeIn :mode="'right'">
-          <NuxtLink to="https://velog.io/@neu5563">Velog로 이동</NuxtLink>
-        </FadeIn>
-        <FadeIn :mode="'right'">
-          <NuxtLink to="https://github.com/maj0rika">Github로 이동</NuxtLink>
-        </FadeIn>
-      </template>
-    </ContentsContainer>
-  </FadeIn>
+    <template v-slot:body>
+      <FadeIn :mode="'right'">
+        <NuxtLink to="https://maj0rika.com/react">REACT 블로그로 이동</NuxtLink>
+      </FadeIn>
+      <FadeIn :mode="'right'">
+        <NuxtLink to="https://velog.io/@neu5563">Velog로 이동</NuxtLink>
+      </FadeIn>
+      <FadeIn :mode="'right'">
+        <NuxtLink to="https://github.com/maj0rika">Github로 이동</NuxtLink>
+      </FadeIn>
+    </template>
+  </ContentsContainer>
 </template>
 
 <style scoped lang="scss"></style>
